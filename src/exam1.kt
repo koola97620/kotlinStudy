3cfe610e97cbbcf94f7157c74e5be30fd5c99c23
Jaeyong <koola97620@nate.com>
2020-04-05 16:36:39 +0900

    kotlin study https://kotlinlang.org/docs/reference/idioms.html basic syntax

--- src/exam1.kt
+++ src/exam1.kt
@@ -1,3 +1,5 @@
+import java.awt.Rectangle
+
 /**
  *
  * @project kotlinexam
@@ -78,7 +80,40 @@ class exam1 {
             println("item at $index is ${items3[index]}")
             index++
         }
+
+        val x = 10
+        val y = 9
+        if (x in 1 .. y+1) {
+            println("fits in range")
+        }
+
+        val list = listOf("a","b","c")
+        if(-1 !in 0..list.lastIndex) {
+            println("-1 is out of range")
+        }
+        if(list.size !in list.indices) {
+            println("list size is out of valid list indice range, too")
+        }
+        for ( x in 1..5) {
+            print(x)
+        }
+        for ( x in 1..10 step 2) {
+            print(x)
+        }
+        println()
+        for(x in 9 downTo 0 step 3) {
+            print(x)
+        }
     }
+
+    fun describe(obj: Any): String =
+        when (obj) {
+            1 -> "One"
+            "hello" -> "greeting"
+            is Long -> "Long"
+            !is String -> "not a string"
+            else -> "unknown"
+        }
 
 
     fun init() {
@@ -95,6 +130,29 @@ class exam1 {
     var t = 0
     fun increment() {
         t += 1
+    }
+
+    fun collectionExam() {
+        val list = listOf("a","b","c")
+        for (item in list) {
+            println(item)
+        }
+
+        when {
+            "orange" in list -> println("juicy")
+            "apple" in list -> println("apple is fine too")
+        }
+
+        val fruits = listOf("apple","banana","kiwifruit")
+        fruits
+            .filter { it.startsWith("a") }
+            .sortedBy { it }
+            .map { it.toUpperCase() }
+            .forEach { println(it) }
+
+        val rectangle = Rectangle(5,2)
+
+
     }
 
 }
