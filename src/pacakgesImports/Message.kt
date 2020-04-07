ceb292a4a5b30a24fee5d277a78b8c771f2e2827
Jaeyong <koola97620@nate.com>
2020-04-07 20:53:50 +0900

    kotlin study https://kotlinlang.org/docs/reference/idioms.html packages and imports

---
+++ src/pacakgesImports/Message.kt
@@ -0,0 +1,12 @@
+package pacakgesImports
+
+/**
+ *
+ * @project kotlinexam
+ * @author choijaeyong on 2020/04/08.
+ * @description
+ */
+class Message {
+    fun messages() {}
+
+}
---
+++ src/pacakgesImports/app.kt
@@ -0,0 +1,5 @@
+package pacakgesImports
+
+import pacakgesImports.Message
+
+
